f05db6e7ca58eca1247ef266312e1676fe1400b3
Jack Huang <jahuang@linkedin.com>
2018-06-01 10:43:14 -0700

    Update

--- vector.py
+++ vector.py
@@ -4,6 +4,10 @@ import numbers
 class Vector:
     def __init__(self, *elements):
         self.elements = elements
+        # convenience attributes for R^2 and R^3
+        self.x = self[0]
+        self.y = self[1]
+        self.z = self[2]
 
     def __getitem__(self, index):
         return self.elements[index]
@@ -20,11 +24,9 @@ class Vector:
         else:
             return sum([a * b for a, b in zip(self.elements, other.elements)])
 
+    # magnitude
     def __abs__(self):
         return math.sqrt(sum([e * e for e in self.elements]))
-
-    def __str__(self):
-        return f"V{self.elements}"
 
     def cross(self, other):
         pass
@@ -32,3 +34,7 @@ class Vector:
     def normalize(self):
         mag = abs(self)
         return Vector(*[e / mag for e in self.elements])
+
+    def __str__(self):
+        return f'V{self.elements}'
+
