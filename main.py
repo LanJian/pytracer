f8025251a74d304116b4ea0a6a0cb8442e1ef4d3
Jack Huang <jahuang@linkedin.com>
2018-06-07 16:41:28 -0700

    Update

--- main.py
+++ main.py
@@ -40,27 +40,3 @@ for u in range(img.size[0]):
 
 img.save('output.png')
 
-
-# v1 = Vector(2, 0, 0)
-# v2 = Vector(1, 2, 0)
-# print(v1[0])
-# print(v1[1])
-# print(v1[2])
-# print(v1.x)
-# print(v1.y)
-# print(v1.z)
-# print(v1 + v2)
-# print(v1 - v2)
-# print(v1 * v2)
-# print(v1 * 10)
-# print(abs(Vector(3, 4, 0)))
-# print(v2.normalize())
-# print(abs(v2.normalize()))
-# print(Vector.J.cross(Vector.K))
-
-# p1 = Point(1, 2, 3)
-# p2 = Point(5, 5, 6)
-# v = Vector(4, 5, 6)
-# print(p1 + v)
-# print(p1 - v)
-# print(p2 - p1)
